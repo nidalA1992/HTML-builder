8f1c8cd3716f5a1433cb790135181755cc68943b
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-05-07 22:09:05 +0500

    feat: solve 01-write-file task

---
+++ 02-write-file/Writer.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('node:process');
+const readline = require('node:readline');
+
+class Writer {
+  onExit () {
+    console.log('Сообщение сохранено! Пока!')
+    process.exit();
+  }
+
+  init(fileName) {
+    const pathName = path.join(__dirname, fileName);
+    const stream = fs.createWriteStream(pathName, 'utf-8');
+    const {stdin: input, stdout: output} = process;
+
+    console.log('Введите сообщение:');
+
+    const read = readline.createInterface({ input, output });
+
+    read.on('line', (input) => {
+      if (input === 'exit') this.onExit();
+
+      stream.write(input + '\n');
+    });
+
+    read.on('close', this.onExit);
+  }
+}
+
+module.exports = Writer;
---
+++ 02-write-file/index.js
@@ -0,0 +1,3 @@
+const Writer = require('./Writer');
+
+new Writer().init('text.txt');
