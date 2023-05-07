81d52b2a4e71106f750e6d415d4912b05dc3cadb
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-05-08 00:47:42 +0500

    feat: solve 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const destDirPath = path.join(__dirname, 'files-copy');
+const sourceDirPath = path.join(__dirname, 'files');
+
+fs.rm(destDirPath, { recursive: true}, (err) => {
+  if(err) console.error(err.message);
+
+  fs.mkdir(
+    destDirPath,
+    {recursive: true},
+    (err) => {
+      if (err) console.error(err.message);
+
+      fs.readdir(sourceDirPath, (err, files) => {
+        if (err) console.error(err.message);
+
+        files.forEach(file => {
+          fs.copyFile(
+            path.join(sourceDirPath, file),
+            path.join(destDirPath, file),
+            (err) => {
+              if (err) console.error(err.message);
+            }
+          );
+        });
+      });
+    }
+  );
+});
+
+
+
+
+
 
