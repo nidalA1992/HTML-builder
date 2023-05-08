514f020e4fca5a8314622be306040a3d9e3970f1
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-05-09 01:19:37 +0500

    fix: add finish fixes

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,9 +4,7 @@ const path = require('path');
 const destDirPath = path.join(__dirname, 'files-copy');
 const sourceDirPath = path.join(__dirname, 'files');
 
-fs.rm(destDirPath, { recursive: true}, (err) => {
-  if(err) console.error(err.message);
-
+fs.rm(destDirPath, { recursive: true}, () => {
   fs.mkdir(
     destDirPath,
     {recursive: true},
