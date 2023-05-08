e2277a04dc8971a729039bd8f236f86cb728d073
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-05-09 01:31:41 +0500

    fix: change split files

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,43 @@
 const path = require('path');
-const Reader = require('./Reader');
+const fs = require('fs');
 
 const filePath = path.join(__dirname, 'text.txt');
+
+class Reader {
+  constructor(path) {
+    this.path = path;
+  }
+
+  concatChunk(chunk) {
+    if(!this.data) {
+      this.data = chunk;
+    } else {
+      this.data += chunk;
+    }
+  }
+
+  onEnd() {
+    console.log(this.data);
+  }
+
+  onError(error) {
+    console.error(new Error(error.message));
+  }
+
+  createStream() {
+    this.stream = fs.createReadStream(this.path, 'utf-8');
+  }
+
+  initHandlers() {
+    this.stream.on('data', this.concatChunk);
+    this.stream.on('end', this.onEnd);
+    this.stream.on('error', this.onError);
+  }
+
+  read() {
+    this.createStream();
+    this.initHandlers();
+  }
+}
 
 new Reader(filePath).read();
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,13 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const EventEmitter = require('events');
 
-const { 
-  copyStyles, 
-  recursiveCopy, 
-  attachComponents, 
-  getComponents 
-} = require('./lib/utils');
-
 const emit = new EventEmitter();
 
 const assets = path.join(__dirname, 'assets');
@@ -46,3 +39,106 @@ emit.on('created', () => {
 emit.on('components-done', (components) => {
   attachComponents(dist, template, components);
 });
+
+function copyStyles(dist, source) {
+  fs.readdir(source, { withFileTypes: true }, (err, data) => {
+    if (err) console.error(err.message);
+
+    data.forEach(item => {
+      if (item.isFile() && path.extname(item.name) === '.css') {
+        const stream = fs.createReadStream(path.join(source, item.name));
+
+        let data = '';
+
+        stream.on('data', (chunk) => {
+          data += chunk;
+        });
+
+        stream.on('end', () => {
+          data += '\n';
+
+          fs.appendFile(path.join(dist, 'style.css'), data, (err) => {
+            if (err) console.error(err.message);
+          });
+        });
+      }
+    });
+  });
+}
+
+function recursiveCopy(dist, source) {
+  fs.readdir(source, { withFileTypes: true }, (err, data) => {
+    if (err) console.error(err.message);
+
+    data.forEach(item => {
+      if (item.isFile()) {
+
+        fs.copyFile(path.join(source, item.name), path.join(dist, item.name), (err) => {
+          if (err) console.error(err.message);
+        });
+
+      } else {
+
+        fs.mkdir(path.join(dist, item.name), (err) => {
+          if (err) console.error(err.message);
+        });
+
+        recursiveCopy(path.join(dist, item.name), path.join(source, item.name));
+      }
+    });
+  });
+}
+
+function attachComponents(dist, template, components) {
+
+  let doc = '';
+
+  const input = fs.createReadStream(template);
+  const output = fs.createWriteStream(path.join(dist, 'index.html'));
+
+  input.on('data', (chunk) => {
+    doc += chunk;
+  });
+
+  input.on('end', () => {
+    
+    for(const [name, component] of Object.entries(components)) {
+      const target = `{{${name}}}`;
+
+      doc = doc.replaceAll(target, component);
+    }
+
+    output.write(doc);
+  });
+}
+
+function getComponents(source, emit) {
+  fs.readdir(source, (err, data) => {
+    if (err) console.error(err.message);
+
+    let comp = {};
+
+    const components = data.filter(item => path.extname(item) === '.html');
+
+    components.forEach((component, i) => {
+      const rs = fs.createReadStream(path.join(source, component));
+
+      const name = component.split('.')[0];
+
+      comp[name] = '';
+
+      rs.on('data', (chunk) => {
+        if (err) console.error(err.message);
+
+        comp[name] += chunk;
+      });
+
+      rs.on('end', () => {
+
+        if (i === components.length - 2) {
+          emit.emit('components-done', comp);
+        }
+      });
+    });
+  });
+}
